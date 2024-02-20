b4ee124247956e7900660738316ff137963eb88a
leroysb <leroybuliro@gmail.com>
2024-02-20 14:22:46 +0300

    field addition

--- internal/database/models/customer.go
+++ internal/database/models/customer.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Customer struct {
 	gorm.Model
-	Name  string `json:"name" gorm:"text;not null;default:null"`
-	Email string `json:"email" gorm:"text;not null;unique"`
-	Phone string `json:"phone" gorm:"text;not null;unique"`
+	Name     string `json:"name" gorm:"text;not null;default:null"`
+	Phone    string `json:"phone" gorm:"text;not null;unique"`
+	Password string `json:"password" gorm:"text;not null;default:null"`
 }
