542514e8f7a97460cdda9418d06bd3c8b40f71c6
leroysb <leroybuliro@gmail.com>
2024-02-22 16:43:49 +0300

    modified order model

--- internal/database/models/order.go
+++ internal/database/models/order.go
@@ -5,11 +5,11 @@ import "gorm.io/gorm"
 type Order struct {
 	gorm.Model
 	Customer   Customer `gorm:"foreignKey:CustomerID"`
-	CustomerID uint
-	Product    Product `gorm:"foreignKey:ProductID"`
-	ProductID  uint
-	Quantity   int     `json:"quantity" gorm:"integer;not null;default:null"`
-	Amount     float64 `json:"amount" gorm:"float;not null;default:null"`
-	Time       string  `json:"time" gorm:"text;not null;default:null"`
-	Status     string  `json:"status" gorm:"text;not null;default:null"`
+	CustomerID uint     `json:"customer_id" gorm:"integer;not null;default:null"`
+	Product    Product  `gorm:"foreignKey:ProductID"`
+	ProductID  uint     `json:"product_id" gorm:"integer;not null;default:null"`
+	Quantity   int      `json:"quantity" gorm:"integer;not null;default:null"`
+	Amount     int      `json:"amount" gorm:"float;not null;default:null"`
+	Time       string   `json:"time" gorm:"text;not null;default:null"`
+	Status     string   `json:"status" gorm:"text;not null;default:null"`
 }
